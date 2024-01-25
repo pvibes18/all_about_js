8203020869c34a46b36bfa43688ba1b437a4ff40
pvibes18 <156800513+pvibes18@users.noreply.github.com>
2024-01-25 09:38:15 +0000

    Data_Types

---
+++ .devcontainer/01_basics/1_test.js
@@ -0,0 +1,14 @@
+console.log("Pallavi");
+const accountId =144553
+let accountEmail ="www.amazon.in"
+var accountPassword = "12345"
+accountCity = "Ahmedabad"
+// accountId = 2 //not allowed
+accountEmail = "www.flipkart.com"
+accountPassword = "98765"
+accountCity = "Powai"
+console.log(accountId);
+console.table([accountEmail , accountId, accountPassword, accountCity])
+let accountState;
+console.log(accountState)
+// prefer not to use var because of issue in block scope and functional scope
