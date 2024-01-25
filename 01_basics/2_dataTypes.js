6bb0594e2f9c0018ceaabec4ae0c69b21d6ba169
pvibes18 <156800513+pvibes18@users.noreply.github.com>
2024-01-25 09:43:12 +0000

    dataTypes

---
+++ 01_basics/2_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello everyone") since we are using nodejs and not brower hence this is not the way to write here
+
+console.log(8+23)
+
+console.log("Enjoy your learning")
+
+let name ="Pallavi" //string
+let age = 19 //number
+let isLoggedIn = false //boolean
+let state //undefined
+let state1 = null
+
+// bigint
+// string => within ""/''
+// boolean => true/false
+// null => standalone value => representation of an empty value
+// undefined =>
+// symbol => unique
+
+
+console.table([typeof name, typeof age, typeof isLoggedIn ,typeof state, typeof state1] )
+
+console.log(typeof undefined); // output:undefined
+console.log(typeof null); // output: object
