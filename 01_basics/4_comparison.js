1b63145f6a6b9e81cc1c15b0f7210f249b45450e
pvibes18 <156800513+pvibes18@users.noreply.github.com>
2024-01-26 10:18:22 +0000

    comparison

---
+++ 01_basics/4_comparison.js
@@ -0,0 +1,15 @@
+//comparing two different data types
+
+console.log("2">1); //true
+console.log("02" > 1 ); //true
+
+console.log(null > 0);  //false
+console.log(null == 0); //false******comparision and equality check works differently in js
+console.log(null >= 0); //true
+
+console.log(undefined == 0); //false
+
+console.log("2"==2);  // true
+console.log("2"===2); //=== chks the datatype also => hence false
+
+
